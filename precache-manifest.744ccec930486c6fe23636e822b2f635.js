a105a2baad9366640f31f500fcc06e25f953043d
Luis Leopardi <supernano1998@gmail.com>
2020-01-31 18:48:26 -0400

    Updates

---
+++ precache-manifest.744ccec930486c6fe23636e822b2f635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee5426838c27156ef94859ca47942266",
+    "url": "/currencyConvertor/index.html"
+  },
+  {
+    "revision": "f52cc1332e16ca7e9eb5",
+    "url": "/currencyConvertor/static/css/main.12805c1f.chunk.css"
+  },
+  {
+    "revision": "c12b597fd5c1d93fd5c5",
+    "url": "/currencyConvertor/static/js/2.6ee756a5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/currencyConvertor/static/js/2.6ee756a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f52cc1332e16ca7e9eb5",
+    "url": "/currencyConvertor/static/js/main.bcaceeb1.chunk.js"
+  },
+  {
+    "revision": "0d7f90f9a8f75bd1cfbe",
+    "url": "/currencyConvertor/static/js/runtime-main.e98867a1.js"
+  }
+]);
